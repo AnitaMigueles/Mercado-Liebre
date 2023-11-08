373c7e954cf6632594e945f88885683c2d2e3f2d
AnitaMigueles <anita.migueles@gmail.com>
2023-11-07 22:34:36 -0300

    Corrigiendo el port

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static('public'));
 
 //Levantar nuestro servidor
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port,()=>console.log('Servidor corriendo en el puerto ${port}'));
 
 /*app.listen(3000, ()=>{
