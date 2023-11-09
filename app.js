8a407c0cd27c2f324b1667bd3a00f3648aff1a3a
AnitaMigueles <anita.migueles@gmail.com>
2023-11-09 19:26:00 -0300

    corrigiendo media queries

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ app.use(express.static('public'));
 const port = process.env.PORT || 3000;
 app.listen(port,()=>console.log('Servidor corriendo en el puerto ${port}'));
 
+
 /*app.listen(3000, ()=>{
-    console.log('Servidor funcionando en el puerto' + PORT);
+    console.log('Servidor funcionando en el puerto' + 3000);
 });*/
 
 app.get('/', (req,res)=>{
